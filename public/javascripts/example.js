dd6851bfa0261bbd8d8f66f5c90653ccfeba9677
Jackie Zhang <viperking@163.com>
2017-01-04 22:34:11 +0800

    added sample codes

--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -5,6 +5,7 @@
 window.onload = function () {
     var testdiv = document.getElementById("testdiv");
     var para = document.createElement("p");
+    para.style.color = "red";
     var info = "nodeName:" + para.nodeName + " nodeType:" + para.nodeType;
     testdiv.appendChild(para);
     var text = document.createTextNode("hello world");
