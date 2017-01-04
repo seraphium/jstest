b44f6cc677dc100187663101512a7d54ff4c6f57
seraphium <viperking@163.com>
2017-01-04 16:34:02 +0800

    first commit

---
+++ public/javascripts/example.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zezhang on 2017/1/4.
+ */
+
+window.onload = function () {
+    var testdiv = document.getElementById("testdiv");
+    var para = document.createElement("p");
+    var info = "nodeName:" + para.nodeName + " nodeType:" + para.nodeType;
+    testdiv.appendChild(para);
+    var text = document.createTextNode("hello world");
+    para.appendChild(text);
+    displayAbbreviations();
+};
+
