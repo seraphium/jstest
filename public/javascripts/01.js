46d9a23aeb0b8f1276202204f9ddc6d1f1651f35
Jackie Zhang <viperking@163.com>
2017-01-08 11:06:46 +0800

    trying jquery event

--- public/javascripts/01.js
+++ public/javascripts/01.js
@@ -22,4 +22,22 @@ $(document).ready(function() {
         .addClass('highlight');
 
     console.log($('div.poem-stanza'));
+
+    $('#switcher-default').addClass('selected');
+    $('#switcher button').click(function() {
+        var bodyClass = this.id.split('-')[1];
+        $('body').removeClass().addClass(bodyClass);
+        $('#switcher button').removeClass('selected');
+        $(this).addClass('selected');
+    });
+
+    $('#switcher h3').click(function() {
+        $('#switcher button').toggleClass('hidden');
+    });
+
+    $('#switcher h3').hover(function() {
+        $(this).addClass('hover');
+    }, function() {
+        $(this).removeClass('hover');
+    });
 });
