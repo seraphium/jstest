ee40c7fc8abd3d45a632efa187d95989178d2440
seraphium <viperking@163.com>
2017-01-09 16:38:39 +0800

    finished basic css animation practise

--- public/javascripts/01.js
+++ public/javascripts/01.js
@@ -129,4 +129,14 @@ $(document).ready(function() {
             $link.text('read more');
         }
         });
+
+    $('p').eq(2)
+        .css('border', '1px solid #333')
+        .click(function() {
+            var $clickedItem = $(this);
+            $clickedItem.next().slideDown('slow', function() {
+                $clickedItem.slideUp('slow');
+            });
+        });
+    $('p').eq(3).css('backgroundColor', '#ccc').hide();
 });
