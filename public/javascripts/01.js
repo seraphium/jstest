8bc065cd9fe4b56ce987800a57a3422f2d909567
Jackie Zhang <viperking@163.com>
2017-01-10 23:41:10 +0800

    trying jquery element creating/moving/wrapping

--- public/javascripts/01.js
+++ public/javascripts/01.js
@@ -3,14 +3,26 @@
  */
 
 $(document).ready(function() {
-    $('div.chapter a[href*="wikipedia"]').attr({
-        rel: 'external',
-        title: function() {
-            return 'Learn more about ' + $(this).text()
-                + ' at Wikipedia.';
-        },
-        id: function(index, oldValue) {
-            return 'wikilink-' + index;
-        }
+    $('<a href="#top">back to top</a>').insertAfter('div.chapter p');
+    $('<a id="top"></a>').prependTo('body');
+
+    var $notes = $('<ol id="notes"></ol>').insertBefore('#footer');
+    $('span.footnote').each(function(index) {
+        $(this)
+            .before([
+                '<a href="#footnote-',
+                index + 1,
+                '" id="context-', index + 1,
+                '" class="context">',
+                '<sup>',
+                index + 1,
+                '</sup></a>'
+            ].join('')).appendTo($notes)
+            .append([
+                '&nbsp;(<a href="#context-',
+                index + 1,
+                '">context</a>)'
+            ].join(''))
+            .wrap('<li id="footnote-' + (index + 1) + '"></li>');
     });
 });
