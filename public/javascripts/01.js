4093a0771c9eb6cefab49e55ac42a986557d1c01
seraphium <viperking@163.com>
2017-01-11 14:04:56 +0800

    finished json/xml get practise

--- public/javascripts/01.js
+++ public/javascripts/01.js
@@ -25,4 +25,92 @@ $(document).ready(function() {
             ].join(''))
             .wrap('<li id="footnote-' + (index + 1) + '"></li>');
     });
+
+
+    $('span.pull-quote').each(function(index) {
+        var $parentParagraph = $(this).parent('p');
+        $parentParagraph.css('position', 'relative');
+        var $clonedCopy = $(this).clone();
+        $clonedCopy
+            .addClass('pulled')
+            .find('span.drop')
+            .html('&hellip;')
+            .end()
+            .text($clonedCopy.text())
+            .prependTo($parentParagraph);
+    });
+
+    $('#letter-a a').click(function(event) {
+        event.preventDefault();
+        $("#dictionary").load("a.html");
+       // alert('Loaded!');
+    });
+
+    $('#letter-b a').click(function(event) {
+        event.preventDefault();
+        $.getJSON("b.json", function(data)  {
+            var html = '';
+            $.each(data, function(entryIndex, entry) {
+                html += '<div class="entry">';
+                html += '<h3 class="term">' + entry.term + '</h3>';
+                html += '<div class="part">' + entry.part + '</div>';
+                html += '<div class="definition">';
+                html += entry.definition;
+                if (entry.quote) {
+                    html += '<div class="quote">';
+                    $.each(entry.quote, function(lineIndex, line) {
+                        html += '<div class="quote-line">' + line + '</div>';
+                    });
+                    if (entry.author) {
+                        html += '<div class="quote-author">' + entry.author + '</div>';
+                    }
+                    html += '</div>';
+                }
+                html += '</div>';
+                html += '</div>';
+            });
+            $('#dictionary').html(html);
+         //   alert('Loaded!');
+
+        });
+    });
+
+    $('#letter-c a').click(function(event) {
+        event.preventDefault();
+        $.getScript('javascripts/c.js');
+    });
+
+    $('#letter-d a').click(function(event) {
+            event.preventDefault();
+            $.get('d.xml', function(data) {
+                $('#dictionary').empty();
+                $(data).find('entry:has(quote[author])').each(function() {
+                    var $entry = $(this);
+                    var html = '<div class="entry">';
+                    html += '<h3 class="term">' + $entry.attr('term');
+                    html += '</h3>';
+                    html += '<div class="part">' + $entry.attr('part');
+                    html += '</div>';
+                    html += '<div class="definition">';
+                    html += $entry.find('definition').text();
+                    var $quote = $entry.find('quote');
+                    if ($quote.length) {
+                        html += '<div class="quote">';
+                        $quote.find('line').each(function () {
+                            html += '<div class="quote-line">';
+                            html += $(this).text() + '</div>';
+                        });
+                        if ($quote.attr('author')) {
+                            html += '<div class="quote-author">';
+                            html += $quote.attr('author') + '</div>';
+                        }
+                        html += '</div>';
+                    }
+                    html += '</div>';
+                    html += '</div>';
+                    $('#dictionary').append($(html));
+                });
+        });
+    });
+
 });
